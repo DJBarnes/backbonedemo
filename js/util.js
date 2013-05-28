f33bcf9b2b7f2f5ebf00667f8a1b3cc140be153a
DJBarnes <barnesdavidj@gmail.com>
2013-05-28 01:50:04 -0400

    Add Util.js to load template files.

---
+++ js/util.js
@@ -0,0 +1,25 @@
+tpl = {
+  templates : {},
+  
+  loadTemplates : function(names, callback) {
+    var self = this;
+    
+    var loadTemplate = function(index) {
+      var name = names[index];
+      $.get('tpl/' + name + '.html', function(data) {
+        that.templates[name] = data;
+        index++;
+        if (index < names.length) {
+          loadTemplate(index);
+        } else {
+          callback();
+        }
+      });
+    }
+    loadTemplate(0);
+  },
+  
+  get : function(name) {
+    return this.templates[name];
+  }
+};
