a10c5236064ae010eb651739e9deee5cec12b6a2
DJBarnes <david.j.barnes@wmich.edu>
2013-07-18 12:25:32 -0400

    Add Authentication to site. Still needs logout.

--- api/index.php
+++ api/index.php
@@ -8,6 +8,8 @@ $app = new \Slim\Slim();
 $app -> get('/items/', 'getItems');
 $app -> put('/items/:id', 'updateItem');
 
+$app -> post('/login/', 'loginAuth');
+
 $app -> run();
 
 function getItems() {
@@ -24,35 +26,67 @@ function getItems() {
 }
 
 function updateItem($id) {
-  $request = \Slim\Slim::getInstance()->request();
-  $body = $request->getBody();
+  $request = \Slim\Slim::getInstance() -> request();
+  $body = $request -> getBody();
   $item = json_decode($body);
   $sql = "UPDATE item set name=:name, quantity=:quantity, description=:description WHERE id=:id";
-  
-  try {
-    $db = getConnection();
-    $stmt = $db -> prepare($sql);
-    $stmt->bindParam("name",$item->name);
-    $stmt->bindParam("quantity",$item->quantity);
-    $stmt->bindParam("description",$item->description);
-    $stmt->bindParam("id",$item->id);
-    $stmt->execute();
-    $db = null;
-    echo json_encode($item);
-  } catch(PDOException $e) {
-    echo '{"error":{"text":' . $e->getMessage() . '}}';
+
+  session_start();
+  if (isset($_SESSION['userId'])) {
+
+    try {
+      $db = getConnection();
+      $stmt = $db -> prepare($sql);
+      $stmt -> bindParam("name", $item -> name);
+      $stmt -> bindParam("quantity", $item -> quantity);
+      $stmt -> bindParam("description", $item -> description);
+      $stmt -> bindParam("id", $item -> id);
+      $stmt -> execute();
+      $db = null;
+      echo json_encode($item);
+    } catch(PDOException $e) {
+      echo '{"error":{"text":' . $e -> getMessage() . '}}';
+    }
+  } else {
+    echo 'You Have Been Logged Out Due To Inactivity';
   }
 
 }
 
+function loginAuth() {
+  $username = $_POST['Username'];
+  $password = $_POST['Password'];
+  $sql = "SELECT * FROM users WHERE username=:username";
+  try {
+    $db = getConnection();
+    $stmt = $db -> prepare($sql);
+    $stmt -> bindParam("username", $username);
+    $stmt -> execute();
+    $User = $stmt -> fetchObject();
+    $db = null;
+    if ($User) {
+      if ($User -> password == $password) {
+        session_start();
+        $_SESSION['userId'] = $User -> id;
+        echo true;
+      } else {
+        echo false;
+      }
+    } else {
+      echo false;
+    }
+  } catch(PDOException $e) {
+    echo '{"error" : {"text" :' . $e -> getMessage() . '}}';
+  }
+}
+
 function getConnection() {
-  $dbhost = "127.0.0.1";
+  $dbhost = "localhost";
   $dbuser = "kvcc";
   $dbpass = "password";
   $dbname = "backbonedemo";
-  $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname",$dbuser,$dbpass);
+  $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
   $dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   return $dbh;
 }
-
 ?>
